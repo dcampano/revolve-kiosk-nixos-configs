c4e416e625c9dc8e784c07000e38923d2bf4f9bd
Davy Campano <dcampano@gmail.com>
2026-02-05 11:30:35 -0500

    Initial Version

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    # Leaving this here in case there is ever a reason that I 
+    # need to pin Chromium to a specific revision
+    nixpkgs-chromium.url = "github:NixOS/nixpkgs/nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-chromium, ... }:
+    let
+      system = "aarch64-linux"; # change to "aarch64-linux" on Raspberry Pi, etc.
+      chromiumOverlay = final: prev: {
+        chromium = (import nixpkgs-chromium {
+          inherit system;
+          config = prev.config;  # carries allowUnfree, etc.
+        }).chromium;
+      };
+    in
+    {
+      nixosConfigurations = {
+        nixos = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            ({ ... }: { nixpkgs.overlays = [ chromiumOverlay ]; })
+            ./configuration.nix
+          ];
+        };
+      };
+    };
+}
+
